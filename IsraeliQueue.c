0259b8bb329d352fe2d2fbbe002defaff4d96daa
Omar Jabarin <omar.ja@campus.technion.ac.il>
2023-05-09 18:08:52 +0300

    Added extensive commentary

--- IsraeliQueue.c
+++ IsraeliQueue.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "IsraeliQueue.h"
+#include <IsraeliQueue.h>
 #include <assert.h>
 #include <string.h>
 #include <math.h>
@@ -11,14 +11,17 @@
 
 typedef enum { FRIEND, RIVAL, NEUTRAL } Relation;
 
-typedef struct{
-	int m_friendshipQuota;
-	int m_rivalryQuota;
+typedef struct{ //we implemented using an array, not a linked list and we use the quota struct to keep track
+	int m_friendshipQuota; // of the friendship and rivalry quotas and the improve field is to so we don't improve
+	int m_rivalryQuota; // the same item twice in the IsraeliQueueImprovePosition function
 	void* m_object;
 	bool improved;
 } Quota;
 
-FriendshipFunction* copyFriendshipFunctions(FriendshipFunction* friendshipFunctions, IsraeliQueueError* errorFlag){
+//Helper function that copies the friendship functions from the original array so we never alter the original array
+//And so we can freely add functions using AddFriendshipMeasure and free the memory when done
+//without affecting userspace.
+static FriendshipFunction* copyFriendshipFunctions(FriendshipFunction* friendshipFunctions, IsraeliQueueError* errorFlag){
 	int size=0;
 	while (friendshipFunctions[size]!=NULL){
 		size++;
@@ -37,7 +40,8 @@ FriendshipFunction* copyFriendshipFunctions(FriendshipFunction* friendshipFuncti
 	return newFriendshipFunctions;
 
 }
-Quota* copyQuotas(Quota* quotas, int size, IsraeliQueueError* errorFlag){
+//Helper function to copy quotas when cloning the IsraeliQueue so that the cloned queue has the same memory.
+static Quota* copyQuotas(Quota* quotas, int size, IsraeliQueueError* errorFlag){
 	Quota* newQuotas = malloc(size*sizeof(Quota));
 	if (newQuotas==NULL){
 	#ifndef NDEBUG
@@ -62,13 +66,15 @@ struct IsraeliQueue_t {
 	Quota* m_quotas;
 };
 
-
-Relation checkRelation(FriendshipFunction* friendshipFunctions,
+//Function which takes two objects and checks their relationship using the constraints defined in the assignment.
+static Relation checkRelation(FriendshipFunction* friendshipFunctions,
  void* firstObject, void* secondObject, int friendshipThreshold, int rivalryThreshold ){
 	
 	Relation currentRelation=NEUTRAL;
 	int sumOfFriendshipPoints=0;
 	int count=0;
+	//Iterates over the friendshipfunctions and checks if one of them is above the friendship threshold
+	//If so it returns a FRIEND Relation immediately.
 	while (friendshipFunctions!=NULL && *friendshipFunctions!=NULL){
 		if ((*friendshipFunctions)(firstObject, secondObject)>friendshipThreshold){
 			currentRelation=FRIEND;
@@ -79,7 +85,7 @@ Relation checkRelation(FriendshipFunction* friendshipFunctions,
 		friendshipFunctions++;
 	}
 	double averageFriendshipPoints=0;
-
+	// If the objects are not friends, we check if they are rivals.
 	if (count!=0){
 	averageFriendshipPoints=(double)sumOfFriendshipPoints/count;
 	}
@@ -87,12 +93,14 @@ Relation checkRelation(FriendshipFunction* friendshipFunctions,
 		currentRelation=RIVAL;
 		return currentRelation;
 	}
+	//If they are not rivals or friends they must be neutral. We assert that this is the case and return neutral.
 	assert(currentRelation==NEUTRAL);
 	return currentRelation;
 
 	}
 
-void** copyObjectArray(void** objects, int size, IsraeliQueueError* errorFlag){
+//Function for copying the items from one queue to another. Used in the clone function.
+static void** copyObjectArray(void** objects, int size, IsraeliQueueError* errorFlag){
 	void** newObjects = malloc(size*sizeof(void*));
 	if (newObjects==NULL){
 	#ifndef NDEBUG
@@ -125,6 +133,8 @@ IsraeliQueue IsraeliQueueCreate(FriendshipFunction *friendshipFunctions,
 	#endif
 		return NULL;
 	}
+	//The errorFlag is used to keep track of errors while calling helper functions. Makes sure we don't leak memory.
+	//And that we return the correct error code.
 	IsraeliQueueError errorFlag=ISRAELIQUEUE_SUCCESS;
 	newQueue->m_friendshipThreshold = friendshipThreshold;
 	newQueue->m_rivalryThreshold = rivalryThreshold;
@@ -222,7 +232,7 @@ void IsraeliQueueDestroy(IsraeliQueue queue){
 		free(queue);
 	return;
 }
-
+//Helper function which checks if two objects are friends and whether the friendship quota is full.
 static bool isAvailableFriend(IsraeliQueue queue, void* secondObject, int i){
   return ((checkRelation(queue->m_friendshipFunctions, queue->m_objects[i],
 			secondObject, queue->m_friendshipThreshold, queue->m_rivalryThreshold)==FRIEND)
@@ -237,10 +247,10 @@ static bool isAvailableRival(IsraeliQueue queue, void* secondObject, int j){
 
 }
 
-void checkInsertLocation(IsraeliQueue queue, void* object, int *friendLocation,
-                         bool *skippedToFriend){
-    bool blocked=false;
+//Helper for IsraeliQueueEnqueue, which checks where we should enqueue the given object.
+static void checkInsertLocation(IsraeliQueue queue, void* object, int *friendLocation, bool *skippedToFriend){
 
+	bool blocked=false; //Initialize to no block. We update this if a block occured.
     for (int i=0; i<((queue->m_size)-1); i++){ //iterates from beginning to end searching for a friend
         *friendLocation=UNINITIALIZED;
         *skippedToFriend=false, blocked=false;
@@ -264,9 +274,11 @@ void checkInsertLocation(IsraeliQueue queue, void* object, int *friendLocation,
 }
 
 
-
-void insertObject(IsraeliQueue queue, void* object, int* friendLocation, bool* skippedToFriend, bool improving){
-	if (*skippedToFriend){
+//Helper function for IsraeliQueueEnqueue, after we found the location to insert the object, we insert it.
+static void insertObject(IsraeliQueue queue, void* object, int* friendLocation, bool* skippedToFriend, bool improving){
+	//If we skipped to a friend, we need to move all objects after the friend one index to the right.
+	//After that we insert the object behind the friend.
+	if (*skippedToFriend){ 
 		for (int i=queue->m_size-1; i>*friendLocation; i--){
 			queue->m_objects[i]=queue->m_objects[i-1];
 			queue->m_quotas[i]=queue->m_quotas[i-1];
@@ -275,7 +287,10 @@ void insertObject(IsraeliQueue queue, void* object, int* friendLocation, bool* s
 		queue->m_quotas[*friendLocation+1].m_friendshipQuota=0;
 		queue->m_quotas[*friendLocation+1].m_rivalryQuota=0;
 		queue->m_quotas[*friendLocation+1].m_object=object;
-		if (!improving){
+		//the improving flag is to keep track of whether the insertion happened during ImprovePositions.
+		//If we're inserting during IsraeliQueueImprovePosition we need to make sure that we don't dequeue and enqueue
+		//The same object twice.
+		if (!improving){ 
 			queue->m_quotas[*friendLocation+1].improved=false;
 		}
 	}
@@ -292,8 +307,8 @@ void insertObject(IsraeliQueue queue, void* object, int* friendLocation, bool* s
 
 }
 
-
-IsraeliQueueError resizeQueue(IsraeliQueue queue){
+//Helper function for IsraeliQueueEnqueue, we resize the queue before we insert a new object.
+static IsraeliQueueError resizeQueue(IsraeliQueue queue){
 	void *tempPtr=queue->m_objects;
 	queue->m_objects=realloc(queue->m_objects, (queue->m_size+1)*sizeof(void*));
 	if (queue->m_objects==NULL){
@@ -317,12 +332,14 @@ IsraeliQueueError resizeQueue(IsraeliQueue queue){
 	return ISRAELIQUEUE_SUCCESS;
 }
 
-int enqueueAndGetIndex(IsraeliQueue queue, void* object, IsraeliQueueError* errorFlag){
+// Helper function that enqueues an object and returns its index. Used during ImprovePositions.
+//So we know where we enqueued an object so we can update its improved flag and restore its quota after we dequeue it.
+static int enqueueAndGetIndex(IsraeliQueue queue, void* object, IsraeliQueueError* errorFlag){
 	int friendLocation=UNINITIALIZED;
 	bool skippedToFriend=false;
 
 	checkInsertLocation(queue, object, &friendLocation, &skippedToFriend);
-	if (resizeQueue(queue)==ISRAELIQUEUE_ALLOC_FAILED){					//Resizing
+	if (resizeQueue(queue)==ISRAELIQUEUE_ALLOC_FAILED){	
 		return ENQUEUE_FAILED;
 		*errorFlag=ISRAELIQUEUE_ALLOC_FAILED;
 	}
@@ -345,10 +362,12 @@ IsraeliQueueError IsraeliQueueEnqueue(IsraeliQueue queue, void* object){
 	int friendLocation=UNINITIALIZED;
 	bool skippedToFriend=false;
 
+	//Checks where we need to insert the object, updates friendLocation and skippedToFriend to inform insertObject.
 	checkInsertLocation(queue, object, &friendLocation, &skippedToFriend);
-	if (resizeQueue(queue)==ISRAELIQUEUE_ALLOC_FAILED){					//Resizing
+	if (resizeQueue(queue)==ISRAELIQUEUE_ALLOC_FAILED){	
 		return ISRAELIQUEUE_ALLOC_FAILED;
 	}
+	//Inserts the object in the queue. After a friend or at the end depending on the received values.
 	insertObject(queue, object, &friendLocation, &skippedToFriend, false);
 	return ISRAELIQUEUE_SUCCESS;
 	}
@@ -404,7 +423,7 @@ bool IsraeliQueueContains(IsraeliQueue queue, void * object){
 		#endif
 		return false;
 	}
-
+	//Iterates over all the items and compares them to the received object using the compare function.
 	for (int i=0; i<queue->m_size-1; i++){
 		if (queue->m_compare(queue->m_objects[i], object)){
 			return true;
@@ -477,8 +496,9 @@ int IsraeliQueueSize(IsraeliQueue queue){
 
 	return queue->m_size-1;
 }
-
-void* IsraeliQueueDequeueAtIndex(IsraeliQueue queue, int index, IsraeliQueueError* errorFlag){
+//Helper function that dequeues an item at a specific index. Used during ImproveQueue
+//because we need to remove from the middle many times.
+static void* IsraeliQueueDequeueAtIndex(IsraeliQueue queue, int index, IsraeliQueueError* errorFlag){
 	if (queue == NULL){
 		#ifndef NDEBUG
 		printf("IsraeliQueueDequeueAtIndex: queue is NULL");
@@ -511,6 +531,9 @@ void* IsraeliQueueDequeueAtIndex(IsraeliQueue queue, int index, IsraeliQueueErro
 	return object;
 }
 
+//Copies the queue and the goes from end to start dequeuing and enqueuing according to the original order
+//Uses improved bool flag to keep track of items that were already improved.
+//This way we don't dequeue and enqueue the same item twice.
 IsraeliQueueError IsraeliQueueImprovePositions(IsraeliQueue queue){
 	if (queue==NULL){
 		#ifndef NDEBUG
@@ -521,7 +544,7 @@ IsraeliQueueError IsraeliQueueImprovePositions(IsraeliQueue queue){
 	if (queue->m_size<=1){
 		return ISRAELIQUEUE_SUCCESS;
 	}
-	IsraeliQueueError errorFlag=ISRAELIQUEUE_SUCCESS;
+	IsraeliQueueError errorFlag=ISRAELIQUEUE_SUCCESS;//To keep track of errors during helper functions
 	IsraeliQueue originalQueue=IsraeliQueueClone(queue);
 	if (originalQueue==NULL){
 		#ifndef NDEBUG
@@ -531,11 +554,11 @@ IsraeliQueueError IsraeliQueueImprovePositions(IsraeliQueue queue){
 	}
 	for (int i=originalQueue->m_size-2; i>=0; i--){
 		void* currentObject=originalQueue->m_objects[i];
-		for (int j=originalQueue->m_size-2; j>=0; j--){
+		for (int j=originalQueue->m_size-2; j>=0; j--){//iterate from end to beginning dequeuing and enqueueing items
 			if ((currentObject==(queue->m_objects[j])) && !(queue->m_quotas[j].improved)){
 				Quota currentQuota=queue->m_quotas[j];
 				IsraeliQueueDequeueAtIndex(queue, j, &errorFlag);
-				if (errorFlag==ISRAELIQUEUE_ALLOC_FAILED){
+				if (errorFlag==ISRAELIQUEUE_ALLOC_FAILED){//boilerplate error handling
 					#ifndef NDEBUG
 					printf("IsraeliQueueImprovePosition: dequeue failed");
 					#endif
@@ -543,7 +566,7 @@ IsraeliQueueError IsraeliQueueImprovePositions(IsraeliQueue queue){
 					return ISRAELIQUEUE_ALLOC_FAILED;
 				}
 				int index=enqueueAndGetIndex(queue, currentObject, &errorFlag);
-				if (errorFlag==ISRAELIQUEUE_ALLOC_FAILED){
+				if (errorFlag==ISRAELIQUEUE_ALLOC_FAILED){//boilerplate error handling
 					#ifndef NDEBUG
 					printf("IsraeliQueueImprovePosition: enqueue failed");
 					#endif
@@ -563,7 +586,9 @@ IsraeliQueueError IsraeliQueueImprovePositions(IsraeliQueue queue){
 	return ISRAELIQUEUE_SUCCESS;
 }
 
-FriendshipFunction* getAllFriendshipFunctions(IsraeliQueue* queues) {
+//Helper function used during merge. Takes an array of queues and returns a new array
+//that contains all of the friendship functions.
+static FriendshipFunction* getAllFriendshipFunctions(IsraeliQueue* queues) {
     int totalSize = 0;
 
     for (IsraeliQueue* queuePointer=queues; *queuePointer != NULL; queuePointer++) {
@@ -598,12 +623,12 @@ FriendshipFunction* getAllFriendshipFunctions(IsraeliQueue* queues) {
 
 
 IsraeliQueue IsraeliQueueMerge(IsraeliQueue* queueArray, ComparisonFunction compare){
-	if (queueArray==NULL || compare==NULL || *queueArray==NULL){
+	if (queueArray==NULL || compare==NULL || (*queueArray==NULL)){
 		#ifndef NDEBUG
 		printf("IsraeliQueueMerge: queueArray or compare is NULL");
 		#endif
 		return NULL;
-	}
+	}//calculate size of resulting queue and prepare to calculate averages
 	int count=0, sizeOfNewQueue=0, sumOfFriendshipThresholds=0, productOfRivalryThresholds=1;
 	while (queueArray[count]!=NULL){
 		sizeOfNewQueue+=(queueArray[count]->m_size-1);
@@ -611,8 +636,8 @@ IsraeliQueue IsraeliQueueMerge(IsraeliQueue* queueArray, ComparisonFunction comp
 		productOfRivalryThresholds*=queueArray[count]->m_rivalryThreshold;
 		count++;
 	}
-	sizeOfNewQueue+=1;
-	FriendshipFunction* friendshipFunctions=getAllFriendshipFunctions(queueArray);
+	sizeOfNewQueue+=1;// preparing to terminate with NULL this is due to how we implemented queues.
+	FriendshipFunction* friendshipFunctions=getAllFriendshipFunctions(queueArray);//get all friendship functions
 	int averageFriendship=(int)ceil((double)sumOfFriendshipThresholds/count);
 	int geometricMeanOfRivalry = (int)ceil(pow((double)abs(productOfRivalryThresholds), 1.0 / count));
 	IsraeliQueue result=IsraeliQueueCreate(friendshipFunctions, compare, averageFriendship, geometricMeanOfRivalry);
@@ -640,133 +665,4 @@ IsraeliQueue IsraeliQueueMerge(IsraeliQueue* queueArray, ComparisonFunction comp
 	}
 	result->m_objects[sizeOfNewQueue-1]=NULL;
 	return result;
-} 
-
-
-int comparison_function_mock(void *obj1, void *obj2) {
-    int id1 = *(int *)obj1;
-    int id2 = *(int *)obj2;
-
-    return !(id1 - id2); /* Fixed this function, should return 1 now if the objects are the same and 0 if they're different.
-			  Anyway, the functions in this test are only useful for testing the first, IsraeliQueue.c part*/
-}
-
-
-int oddEvenFriendshipFunction(void* firstObject, void* secondObject){
-	/* In this test after we initialize the queue we enqueue even numbers so the parity practically
-	depends only on the object already in the queue. We define the friendship threshold to be 3
-	and rivalry threshold to be -1 */
-
-	if (((*(int*)firstObject)+(*(int*)secondObject))%2==0){
-		return 4; /* 4 is over the friendship threshold(=3), which means if the first object is even
-					the enqueued object is always his friend */
-	}
-
-	else {
-		return -2; /* -2 is under the rivalry threshhold(=-1), which means if the first object is odd
-					the enqueued object is always his rival */
-					
-	}
-
-
-}
-
-int main(){
-	FriendshipFunction functions[]={NULL};
-	IsraeliQueue nullTester=IsraeliQueueCreate(functions, comparison_function_mock, 1, 1);
-	void *nullptr;
-
-	IsraeliQueueEnqueue(nullTester, NULL);
-	IsraeliQueueEnqueue(nullTester, NULL);
-	IsraeliQueueEnqueue(nullTester, NULL);
-	nullptr=IsraeliQueueDequeue(nullTester);
-	nullptr=IsraeliQueueDequeue(nullTester);
-	nullptr=IsraeliQueueDequeue(nullTester);
-	
-	printf("%p", nullptr);
-	// test 1
-
-	/* iterations: 
-	1 2 3 4 5 6 7 8 9 10 11 12 <-original
-	1 12 2 3 4 5 6 7 8 9 10 11
-	1 11 12 2 3 4 5 6 7 8 9 10
-	1 10 11 12 2 3 4 5 6 7 8 9
-	1 9 10 11 12 2 3 4 5 6 7 8
-	1 8 9 10 11 12 2 3 4 5 6 7
-	1 8 7 9 10 11 12 2 3 4 5 6
-	1 8 6 7 9 10 11 12 2 3 4 5
-	1 8 5 6 7 9 10 11 12 2 3 4
-	1 8 4 5 6 7 9 10 11 12 2 3
-	1 8 3 4 5 6 7 9 10 11 12 2
-	1 8 3 2 4 5 6 7 9 10 11 12
-	8 3 1 2 4 5 6 7 9 10 11 12
-	
-	 */ /*
-	int arr[]={1,2,3,4,5,6,7,8,9,10,11,12};
-	FriendshipFunction functions[]={NULL};
-	IsraeliQueue queue=IsraeliQueueCreate(functions, comparison_function_mock, 0, 0);
-	
-	for (int i=0; i<12; i++){
-		IsraeliQueueEnqueue(queue, &arr[i]);
-		
-	}
-	IsraeliQueueAddFriendshipMeasure(queue, mockfriendshipfunction);
-	IsraeliQueueImprovePositions(queue);
-	printf("Test1:\nYour output: ");
-	for (int i=0; i<12; i++){
-		int s=*(int*)IsraeliQueueDequeue(queue);
-		printf("%d ", s);
-	}
-	printf("\nExpected:    8 3 1 2 4 5 6 7 9 10 11 12\n");
-	IsraeliQueueDestroy(queue);
-*/
-	// test 2
-	/*iterations:
-	1:  *4
-	2:  4 *3
-	3:  4 *4 3
-	4:  4 *1 4 3
-	5:  4 *3 1 4 3
-	6:  4 *4 3 1 4 3
-	7:  4 4 *2 3 1 4 3
-	8:  4 4 *2 2 3 1 4 3
-	9:  4 4 *3 2 2 3 1 4 3
-	10: 4 4 *1 3 2 2 3 1 4 3
-	11: 4 4 *4 1 3 2 2 3 1 4 3
-	12: 4 4 4 *5 1 3 2 2 3 1 4 3
-	
-	
-	*/
-/*
-	queue=IsraeliQueueCreate(functions, comparison_function_mock, 0, 0);
-	for (int i=0; i<4; i++){
-		IsraeliQueueEnqueue(queue, &arr[i]);
-	}
-	IsraeliQueue p=IsraeliQueueClone(queue);
-	IsraeliQueueImprovePositions(queue);
-	int *five=malloc(sizeof(int));
-	*five=5;
-	IsraeliQueueEnqueue(queue, five);
-	IsraeliQueue s=IsraeliQueueClone(p);
-	IsraeliQueue f=IsraeliQueueClone(p);
-	for (int j=0; j<2; j++){
-		IsraeliQueueDequeue(p);
-	}
-	for (int k=0; k<3; k++){
-		IsraeliQueueDequeue(s);
-	}
-	IsraeliQueue e=IsraeliQueueCreate(functions, comparison_function_mock, 0, 0);
-	IsraeliQueueAddFriendshipMeasure(f, mockfriendshipfunction);
-	IsraeliQueue q_arr[]={queue, p, s, f, e, e, NULL};
-	IsraeliQueue result=IsraeliQueueMerge(q_arr, comparison_function_mock);
-	printf("Test2:\nYour output: ");
-	for (int i=0; i<12; i++){
-		int s=*(int*)IsraeliQueueDequeue(result);
-		printf("%d ", s);
-	}
-	printf("\nExpected:    4 4 4 5 1 3 2 2 3 1 4 3\n");
-*/
-
-	
-	return 0;
 } 
